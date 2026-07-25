88ce193c813cf26b02ff287ff9a6787d5fe6f942
Soft_Potato <lin.benjamin1@gmail.com>
2026-07-25 13:34:32 -0700

    Refactored shop list options to be more dynamic and easily configurable, though adds some potentially bad cyclic dependencies

--- scripts/ui/linear_upgrade_button.gd
+++ scripts/ui/linear_upgrade_button.gd
@@ -1,43 +1,9 @@
-extends Control
+class_name LinearUpgradeButton
+extends UpgradeButton
 
-@export var generator_id: String = ""
-@export var button_text: String = ""
-@export var initial_reveal_threshold: int = 0
-const button_text_format = "%s for %s"
+@export var increment: int = 10
 
-var numberOfUpgrades: int = 0
-
-# Linear cost algorithmn
-@export var current_cost = 10
-@export var increment_per_upgrade = 10
-
-func _ready() -> void:
-	if initial_reveal_threshold > 0:
-		visible = false
-		get_node("TextureButton").set_visible(false)
-		get_node("TextureButton/Text").set_visible(false)
-	
-	# Initialize the button text
-	get_node("TextureButton/Text").set_text(button_text_format % [button_text, current_cost])
-	
-	# Add listener to running score
-	GameCounter.score_changed.connect(check_if_ready)
-
-func check_if_ready(current_feathers: int, lifetime_feathers: int):
-	if !visible:
-		if current_feathers > initial_reveal_threshold:
-			visible = true
-			get_node("TextureButton").set_visible(true)
-			get_node("TextureButton/Text").set_visible(true)
-	
-	if (current_feathers >= current_cost):
-		get_node("TextureButton").set_disabled(false)
-	else:
-		get_node("TextureButton").set_disabled(true)
-
-func _on_texture_button_pressed() -> void:
-	if GameCounter.score >= current_cost:
-		GameCounter.subtract_score(current_cost)
-		emit_signal("generator_upgraded", generator_id, current_cost)
-		current_cost += increment_per_upgrade
-		get_node("TextureButton/Text").set_text(button_text_format % [button_text, current_cost])
+func _register_next_upgrade() -> void:
+	cost += increment
+	recalculate_text()
+	pass
--- scripts/ui/upgrade_menu.gd
+++ scripts/ui/upgrade_menu.gd
@@ -1,11 +1,53 @@
+class_name UpgradeMenu
 extends Control
 
+@export var locked_upgrades: Array[UpgradeButton]
+var unlocked_upgrades: Array[UpgradeButton]
+var purchased_upgrades: Array[UpgradeButton]
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	GameCounter.score_changed.connect(_check_locked_upgrades)
+	_check_locked_upgrades(GameCounter.score, GameCounter.totalFeathersAccumulated)
 
+# Rendering Logic
+#func refresh_menu():
+	#for child in $ScrollContainer/VBoxContainer.get_children():
+		#$ScrollContainer/VBoxContainer.remove_child(child)
+	#
+	#for upgrade in unlocked_upgrades:
+		#upgrade.reparent($ScrollContainer/VBoxContainer)
+		##$ScrollContainer/VBoxContainer.add_child(upgrade)
+		
+# Adding New Upgrade To Render
+func _check_locked_upgrades(current_feathers: int, feathers_lifetime: int) -> void:
+	locked_upgrades.filter(
+		func(upgrade): 
+			if upgrade.render_cost <= current_feathers:
+				unlocked_upgrades.append(upgrade)
+				upgrade.reparent($ScrollContainer/VBoxContainer)
+				return false
+			return true
+	)
+	
+	#refresh_menu()
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+# Performance wise, probably not good. But we don't need to worry too much about scaling
+# at this scale of 10-20 options
+func remove_upgrade(buttonToRemove: UpgradeButton) -> void:
+	unlocked_upgrades.filter(func (upgrade: UpgradeButton) -> bool:
+			return upgrade != buttonToRemove
+	)
+	$ScrollContainer/VBoxContainer.remove_child(buttonToRemove)
+	purchased_upgrades.append(buttonToRemove)
+
+# This callback method is primarily for registering the next upgrade once
+# the previous one is selected
+func register_next_unlocked_upgrade(upgrade: UpgradeUtils.Upgrade):
+	unlocked_upgrades.append(upgrade)
+	_sort_unlocked_upgrades()
+	
+func _sort_unlocked_upgrades() -> void:
+	unlocked_upgrades.sort_custom(_custom_upgrade_sort)
+	
+func _custom_upgrade_sort(a: UpgradeUtils.Upgrade, b: UpgradeUtils.Upgrade):
+	return a.cost < b.cost
