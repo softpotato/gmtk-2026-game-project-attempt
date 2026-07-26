d45233101adf4a199879cbf2f6086618b80e4e24
bgiangkl <bgiang099@gmail.com>
2026-07-26 03:51:00 -0700

    HATS

--- scripts/duck.gd
+++ scripts/duck.gd
@@ -3,7 +3,20 @@ extends Control
 @onready var duck_sound: AudioStreamPlayer2D = $AudioStreamPlayer2D
 @onready var duck_animation: AnimatedSprite2D = $AnimatedSprite2D
 
+@onready var baseback: Sprite2D = $basecapback
+@onready var basefront: Sprite2D = $basecapfront
+@onready var beanback: Sprite2D = $beanback
+@onready var beanfront: Sprite2D = $beanfront
+@onready var bow: Sprite2D = $bow
+@onready var leaf: Sprite2D = $leaf
+@onready var tophat: Sprite2D = $tophat
+@onready var wizback: Sprite2D = $wizback
+@onready var wizfront: Sprite2D = $wizfront
+
+
 @export var play_button: TextureButton
+
+var pat_count: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,6 +28,37 @@ func _on_button_pressed() -> void:
 	duck_animation.stop()
 	duck_animation.play()
 	duck_sound.play()
+	
+	pat_count += 1
+	
+	var hat_check = pat_count % 21
+	
+	if hat_check < 3:
+		wizback.visible = false
+		wizfront.visible = false
+	elif hat_check < 6:
+		baseback.visible = true
+		basefront.visible = true
+	elif hat_check < 9:
+		baseback.visible = false
+		basefront.visible = false
+		beanback.visible = true
+		beanfront.visible = true
+	elif hat_check < 12:
+		beanback.visible = false
+		beanfront.visible = false
+		bow.visible = true
+	elif hat_check < 15:
+		bow.visible = false
+		leaf.visible = true
+	elif hat_check < 18:
+		leaf.visible = false
+		tophat.visible = true
+	elif hat_check < 21:
+		tophat.visible = false
+		wizback.visible = true
+		wizfront.visible = true
+		
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
