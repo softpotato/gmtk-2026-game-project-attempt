e20df955f4f29a781cd363fe8296153bee0f43a3
Soft_Potato <lin.benjamin1@gmail.com>
2026-07-25 17:35:10 -0700

    inventory

--- scripts/globals/global_upgrades.gd
+++ scripts/globals/global_upgrades.gd
@@ -1,4 +1,15 @@
 extends Node
 
+signal upgrade_unlocked(upgrade_id)
+
+var unlocked_upgrades: Dictionary[String, int] = {}
+
 func add_upgrade(upgrade_id: String) -> void:
-	pass
+	if unlocked_upgrades.has(upgrade_id):
+		unlocked_upgrades.set(upgrade_id, unlocked_upgrades.get(upgrade_id))		
+	else:
+		unlocked_upgrades.set(upgrade_id, 1)
+		emit_signal("upgrade_unlocked", upgrade_id)
+
+func is_upgrade_exist(upgrade_id: String) -> void:
+	return unlocked_upgrades.has(upgrade_id)
---
+++ scripts/inventory.gd
@@ -0,0 +1,12 @@
+class_name Inventory
+extends Control
+
+@export var inventory_items: Dictionary[String, Item] = {}
+
+func _ready() -> void:
+	GlobalUpgrades.upgrade_unlocked.connect(process_new_upgrades)
+
+func process_new_upgrades(upgrade_id: String):
+	if inventory_items.has(upgrade_id):
+		var item_icon = inventory_items.get(upgrade_id)
+		item_icon.reparent($Panel/MarginContainer/Panel/MarginContainer/GridContainer)
