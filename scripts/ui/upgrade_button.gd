1b969ef959e94cebb16d670d5a6ca691179e3ef8
Soft_Potato <lin.benjamin1@gmail.com>
2026-07-26 02:05:36 -0700

    Buttons Fix

--- scripts/ui/upgrade_button.gd
+++ scripts/ui/upgrade_button.gd
@@ -2,30 +2,27 @@ class_name UpgradeButton
 extends Control
 
 @export var upgrade_id: String
-@export var play_button: TextureButton
+@export var play_button: Button
 
 func _ready() -> void:
 	GameCounter.score_changed.connect(_update_state)
 	_update_text()
 	_update_state(GameCounter.score, GameCounter.totalFeathersAccumulated)
-	play_button.button_down.connect(_on_button_down)
-	play_button.button_up.connect(_on_button_up)
 	play_button.focus_mode = Control.FOCUS_NONE
 
 func _update_text() -> void:
-	$TextureButton/Text.text = GlobalUpgrades.get_cost_label(upgrade_id)
+	$Button.text = GlobalUpgrades.get_cost_label(upgrade_id)
 
 # Listen for total feathers
 func _update_state(current_feathers: int, _feathers_lifetime: int) -> void:
 	if current_feathers >= GlobalUpgrades.get_cost(upgrade_id):
-		$TextureButton.disabled = false
-		$TextureButton.modulate = Color(1, 1, 1, 1)
+		$Button.disabled = false
+		$Button.modulate = Color(1, 1, 1, 1)
 	else:
-		$TextureButton.disabled = true
-		$TextureButton.modulate = Color(0.5, 0.5, 0.5, 1)
+		$Button.disabled = true
+		$Button.modulate = Color(0.5, 0.5, 0.5, 1)
 
-# When the button is pressed
-func _on_texture_button_pressed() -> void:
+func _on_button_pressed() -> void:
 	var cost = GlobalUpgrades.get_cost(upgrade_id)
 	
 	if GameCounter.score >= cost:
@@ -33,8 +30,10 @@ func _on_texture_button_pressed() -> void:
 		GameCounter.subtract_score(cost)
 		_update_text()
 
-func _on_button_down() -> void:
+
+func _on_button_button_down() -> void:
 	Input.set_custom_mouse_cursor(Cursor.click_cursor, Input.CURSOR_IBEAM, Vector2(35, 35))
 
-func _on_button_up() -> void:
+
+func _on_button_button_up() -> void:
 	Input.set_custom_mouse_cursor(Cursor.default_cursor, Input.CURSOR_ARROW, Vector2(35, 35))
