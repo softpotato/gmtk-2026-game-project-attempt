90ed166bd41fa6a573c2f30e6f09338325e7d836
Soft_Potato <lin.benjamin1@gmail.com>
2026-07-25 18:24:48 -0700

    Update Font + Minimized Inventory

--- scripts/inventory.gd
+++ scripts/inventory.gd
@@ -10,3 +10,14 @@ func process_new_upgrades(upgrade_id: String):
 	if inventory_items.has(upgrade_id):
 		var item_icon = inventory_items.get(upgrade_id)
 		item_icon.reparent($Panel/MarginContainer/Panel/MarginContainer/GridContainer)
+
+var expanded = false
+
+func _on_texture_button_pressed() -> void:
+	if expanded:
+		position.y = 220
+	else:
+		position.y = 720
+		
+	expanded = !expanded
+	position.rotated(PI / 2)
